30d72bea6f4edcd25c54d4f9e4d929d4dabcf108
dariwala <daeeshehab@gmail.com>
2019-08-08 15:53:57 +0600

    Forgot to change 24 hours pharmacy so changed submit button color and upper margin

--- app/Modules/Pharmacy/Resources/Views/pharmacy_edit.blade.php
+++ app/Modules/Pharmacy/Resources/Views/pharmacy_edit.blade.php
@@ -114,11 +114,9 @@
                                                     <input type="file" id="pharmacy_photo" name="pharmacy_photo" class="dropify"/>
                                                 </div>
                                             </div>
-                                            <div class=" uk-width-medium-1-1 ">
-                                                <div class="uk-float-right uk-margin-top">
-                                                    <button type="submit" class="md-btn md-btn-primary" >Submit</button>
-                                                </div>
-                                            </div>
+                                        </div>
+                                        <div class="uk-float-right uk-margin-top">
+                                            <button type="submit" class="md-btn md-btn-primary" style="background: #FD0100;color: #fff;">Submit</button>
                                         </div>
                                     {!! Form::close() !!}
                                 </li>
@@ -219,11 +217,9 @@
                                                 </div> 
                                             </div>
                                         </div>
-                                        <div class="uk-width-medium-1-1">
-                                            <div class="uk-float-right uk-margin-top">
-                                                <button type="submit" class="md-btn md-btn-primary" >Submit</button>
-                                            </div>
-                                        </div>
+                                    </div>
+                                    <div class="uk-float-right uk-margin-top">
+                                        <button type="submit" class="md-btn md-btn-primary" style="background: #FD0100;color: #fff;">Submit</button>
                                     </div>
                                 {!! Form::close() !!}
                                 </li>
--- app/Modules/Pharmacy/Resources/Views/pharmacy_product_add.blade.php
+++ app/Modules/Pharmacy/Resources/Views/pharmacy_product_add.blade.php
@@ -37,11 +37,9 @@
                                 </div>
                             </div>
                         </div>
-                        <div class=" uk-width-medium-1-1 ">
-                            <div class="uk-float-right uk-margin-top">
-                               <button type="submit" class="md-btn md-btn-primary" >Submit</button>
-                            </div>
-                        </div>
+                    </div>
+                    <div class="uk-float-right uk-margin-top">
+                        <button type="submit" class="md-btn md-btn-primary" style="background: #FD0100;color: #fff;">Submit</button>
                     </div>
                 {!! Form::close() !!}
                 </div>
